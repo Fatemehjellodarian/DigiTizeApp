2c2117820bff2bc4fe09f71347525c17b95c2a46
Fatemeh Jellodarian <fatemehjellodarian@gmail.com>
2023-11-14 14:12:51 +0330

    complete sidebar

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,12 @@ module.exports = {
   
       },
     },
-
-    plugins: [],
+   
+    plugins: [
+      require("@tailwindcss/forms")({
+        strategy: 'base', // only generate global styles
+        strategy: 'class', // only generate classes
+      }),
+    ],
+    
   }
